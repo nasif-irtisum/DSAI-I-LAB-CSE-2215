aa8e738b1a2f97d18e27224a72d621494321d7a8
nirtisum-cs <nirtisum.cs@gmail.com>
2026-03-15 08:05:38 +0600

    Add BubbleSort

---
+++ Week 1/bubbleSort0base.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printArray (int *arr, int n)
+{
+    for (int i=0; i<n; i++) {
+        if (i==n-1) cout << arr [i] << endl;
+        else cout << arr [i] << " ";
+    }
+}
+
+void bubbleSort0Base(int *arr, int n)
+{
+    for (int i=0; i<n-1; i++) {
+        for (int j=0; j<n-1-i; j++) {
+            if (arr[j]>arr[j+1]) {
+                int temp = arr [j];
+                arr[j] = arr [j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+int main ()
+{
+    int n; cin >> n;
+    int arr [n];
+
+    for (int i=0; i<n; i++) cin >> arr[i];
+
+    cout << "Before Sorting: ";
+    printArray(arr, n);
+
+    bubbleSort0Base(arr, n);
+
+    cout << "After Sorting: ";
+    printArray(arr, n);
+
+    return 0;
+
+
+}
