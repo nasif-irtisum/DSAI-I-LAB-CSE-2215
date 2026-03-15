ddcd1adfc0f48185fab4744c2d4679f23471bbf6
nirtisum-cs <nirtisum.cs@gmail.com>
2026-03-15 08:09:25 +0600

    make more efficient bubble sort

--- Week 1/bubbleSort0base.cpp
+++ Week 1/bubbleSort0base.cpp
@@ -12,13 +12,16 @@ void printArray (int *arr, int n)
 void bubbleSort0Base(int *arr, int n)
 {
     for (int i=0; i<n-1; i++) {
+        bool isSwap = true;
         for (int j=0; j<n-1-i; j++) {
             if (arr[j]>arr[j+1]) {
                 int temp = arr [j];
                 arr[j] = arr [j+1];
                 arr[j+1] = temp;
+                isSwap = false;
             }
         }
+        if (isSwap) break;
     }
 }
 int main ()
