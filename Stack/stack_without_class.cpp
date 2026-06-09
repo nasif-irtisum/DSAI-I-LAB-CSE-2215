37cf005bfe45f82e254e0c397e5b7f5ff4a2e02a
nirtisum-cs <nirtisum.cs@gmail.com>
2026-06-09 23:10:49 +0600

    stack without class

---
+++ Stack/stack_without_class.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int const sz = 5;
+int arr[sz];
+int top = -1;
+
+void push (int n)
+{
+    if (top+1==sz) {
+        cout << "Stack Overflow!" << endl;
+        return;
+    }
+    top++;
+    arr[top] = n;
+}
+int pop ()
+{
+    if (top==-1) {
+        cout << "Stack Underflow!" << endl;
+        return -1;
+    }
+    int x = arr [top];
+    top--;
+    return x;
+}
+void printStack ()
+{
+    for (int i=0; i<sz; i++) {
+        (i+1==sz) ? cout << arr[i] << endl : cout << arr [i] << " ";
+    }
+}
+
+int main ()
+{
+    for (int i=1; i<=10; i++) push(i);
+    printStack();
+    cout << "Pop: " << pop() << endl;
+    printStack();
+}
